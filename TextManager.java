efd9c2e7def33758ed4446535ceaa13b95f9960d
HelloWorld190 <131030642+HelloWorld190@users.noreply.github.com>
2024-01-03 14:07:27 -0500

    started a new project!

---
+++ TextManager.java
@@ -0,0 +1,8 @@
+package ConsoleSolitair;
+class TextManager {
+    public static final String ANSI_RED = "\u001B[31m";;
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static void initializeClass() {
+        
+    }
+}
